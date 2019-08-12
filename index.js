7392f57ca32d86a82330cad371672ef1ecb30cc5
4vediaayush <4vedi.aayush@gmail.com>
2019-08-12 22:07:41 +0530

    Add styling and proper route navigations

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ var hotels = [
     {name: 'Grand Mercure Bangalore', image:'https://lh3.googleusercontent.com/p/AF1QipPnaPc7H5JX1zJYEWeNZh1RdYB_iauTgPI-nEaZ=w592-h404-n-k-rw-no-v1'},
     {name: 'La Marvella', image:'https://lh3.googleusercontent.com/p/AF1QipM7KZhlFpLiSZwjSLL-E9tRlzey9MRNdjaRRJ6u=w592-h404-n-k-rw-no-v1'},
     {name: 'Hotel Davanam Sarovar Portico Suites', image:'https://lh3.googleusercontent.com/p/AF1QipPGnA_UFw44sS5u2bTdKA8rP07WYPFCrjHFGgoy=w592-h404-n-k-rw-no-v1'},
+    {name: 'The Elanza Hotel', image:'https://lh3.googleusercontent.com/p/AF1QipPjN1pQ00OOS2aMxAvnAwD7rFjQg_UH1pputFPL=w592-h404-n-k-rw-no-v1'},
+    {name: 'Grand Mercure Bangalore', image:'https://lh3.googleusercontent.com/p/AF1QipPnaPc7H5JX1zJYEWeNZh1RdYB_iauTgPI-nEaZ=w592-h404-n-k-rw-no-v1'},
+    {name: 'La Marvella', image:'https://lh3.googleusercontent.com/p/AF1QipM7KZhlFpLiSZwjSLL-E9tRlzey9MRNdjaRRJ6u=w592-h404-n-k-rw-no-v1'},
+    {name: 'La Marvella', image:'https://lh3.googleusercontent.com/p/AF1QipM7KZhlFpLiSZwjSLL-E9tRlzey9MRNdjaRRJ6u=w592-h404-n-k-rw-no-v1'},
+    {name: 'La Marvella', image:'https://lh3.googleusercontent.com/p/AF1QipM7KZhlFpLiSZwjSLL-E9tRlzey9MRNdjaRRJ6u=w592-h404-n-k-rw-no-v1'},
+    {name: 'La Marvella', image:'https://lh3.googleusercontent.com/p/AF1QipM7KZhlFpLiSZwjSLL-E9tRlzey9MRNdjaRRJ6u=w592-h404-n-k-rw-no-v1'},
 ];
 //home page
 app.get('/', (req,res)=>{ 
