294ba18688393ea816e8e34541c8e6ce814db026
4vediaayush <aayush.chaturvedi@razorpay.com>
2019-08-23 23:06:02 +0530

    Add comments and styling

--- index.js
+++ index.js
@@ -3,44 +3,43 @@ var express     = require('express'),
     app         = express(),
     mongoose    = require('mongoose'),
     Hotel       = require('./models/hotel'),
+    Comment     = require('./models/comment')
     seedDB      = require('./seeds')
 
-seedDB();
 mongoose.connect('mongodb://localhost/yelp_hotel');    
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname+ '/public'));
 app.set('view engine','ejs');
+seedDB();
 
 app.get('/', (req,res)=>{ 
     res.render('home'); 
 });
+//INDEX
 app.get('/hotels', (req,res)=>{
     Hotel.find({},(err, allHotels)=>{
         if(err){
             console.log(err);
         }else{
-            res.render('index',{hotels: allHotels});
+            res.render('hotels/index',{hotels: allHotels});
         }
     });
 });
-
+//NEW
 app.post('/hotels', (req,res)=>{
-    var newHotel = {
-        name: req.body.name,
-        image: req.body.image,
-        description: req.body.description
-    };
-    Hotel.create(newHotel,(err,newHotel)=>{
+    Hotel.create(req.body.hotel,(err, hotel)=>{
         if(err){
             console.log(err);
         }else{
-            console.log('SUCCESSFULLY created entry', newHotel);
+            console.log('SUCCESSFULLY created entry', hotel);
         }
     });
     res.redirect('/hotels');
 });
 //order is important here!!!
+//CREATE 
 app.get('/hotels/new', (req,res)=>{
-    res.render('new');
+    res.render('hotels/new');
 });
 //SHOW
 app.get('/hotels/:id',(req,res)=>{
@@ -48,8 +47,45 @@ app.get('/hotels/:id',(req,res)=>{
         if(err){
             console.log(err);
         }else{
-            res.render('show',{hotel: found});
-            console.log('added comments to: ',found);
+            res.render('hotels/show',{hotel: found});
+            // console.log('added comments to: ',found);
+        }
+    });
+});
+// ============================
+// COMMENTS ROUTES
+// ============================
+//CREATE
+app.get('/hotels/:id/comments/new',(req,res)=>{
+    Hotel.findById(req.params.id, (err, hotel)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.render('comments/new', {hotel: hotel});
+        }
+    });
+});
+//NEW
+app.post('/hotels/:id/comments',(req,res)=>{
+    //lookup for hotel using id
+    //create new comment
+    //connect comment to hotel
+    //redirect to show page
+    Hotel.findById(req.params.id, (err, hotel)=>{
+        if(err){
+            console.log(err);
+            res.redirect('/hotels/'+req.params.id);
+        }else{
+            Comment.create(req.body.comment, (err, comment)=>{
+                if(err){
+                    console.log(err);
+                }else{
+                    hotel.comments.push(comment);
+                    hotel.save();
+                    console.log('Created comment!',comment);
+                    res.redirect('/hotels/' + hotel._id);
+                }
+            });
         }
     });
 });
