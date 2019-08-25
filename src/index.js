9ffc713b8ddd37cbbd8963f4d8cefe5f1050adf3
4vediaayush <aayush.chaturvedi@razorpay.com>
2019-08-25 21:41:52 +0530

    Added Components- Card & CardArray to render the list of rangers

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Hello from './Hello';
+// import Hello from './Hello';
+// import Card from './Card' //ATOM
+import CardArray from './CardArray'; //ELEMETS
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { rangers } from './rangers';
 
-ReactDOM.render(<Hello greetings={'Hello React Ninja'} />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+                <div>
+                   <CardArray rangers = {rangers}/>
+                </div>
+, document.getElementById('root'));
 serviceWorker.unregister();
