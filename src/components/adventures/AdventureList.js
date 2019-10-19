533c76510ac6c938ffeebde59e9f06eb334a3d31
4vediaayush <aayush.chaturvedi@razorpay.com>
2019-10-19 10:24:07 +0530

    Add forms for auth

--- src/components/adventures/AdventureList.js
+++ src/components/adventures/AdventureList.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import AdventureSummary from './AdventureSummary';
+import { Route, BrowserRouter, Switch, NavLink } from 'react-router-dom'
 
 const AdventureList = () => {
   let adventures = [
     {
+      'id': 1,
       'title': 'Playing piano at my home',
       'char': 'Oswald',
       'when' : '3rd September, 7pm'
     },
     {
+      'id': 2,
       'title': 'Getting 3 marshmellos with my tea',
       'char': 'Henry',
       'when' : '3rd September, 4pm'
     },
     {
+      'id': 3,
       'title': 'Riding bicycle',
       'char': 'Daisy',
       'when' : '3rd September, 2am'
@@ -21,11 +25,14 @@ const AdventureList = () => {
   ]
   let items = adventures.map( (adv) =>{
     return(
-      <AdventureSummary
-        title = {adv.title}
-        char = {adv.char}
-        when = {adv.when}
-      />
+      <NavLink exact to={"/adv/" + adv.id}>
+        <AdventureSummary
+          id = {adv.id}
+          title = {adv.title}
+          char = {adv.char}
+          when = {adv.when}
+        />
+      </NavLink>
     )
   })
   return (
